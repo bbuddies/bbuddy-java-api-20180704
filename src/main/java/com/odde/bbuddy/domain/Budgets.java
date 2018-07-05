d64d399fd3dcbe66a0c49b20c255a83a73d47381
piya_l <piya.tae@gmail.com>
2018-07-05 19:08:31 +0700

    My legacy code

--- src/main/java/com/odde/bbuddy/domain/Budgets.java
+++ src/main/java/com/odde/bbuddy/domain/Budgets.java
@@ -4,6 +4,7 @@ import com.odde.bbuddy.repository.Budget;
 import com.odde.bbuddy.repository.BudgetRepository;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Service
@@ -21,4 +22,45 @@ public class Budgets {
     public List<Budget> all() {
         return budgetRepository.findAll();
     }
+
+    public Double searchBudget(String startDate, String endDate) {
+        LocalDate start = LocalDate.parse(startDate);
+        LocalDate end = LocalDate.parse(endDate);
+
+        List<Budget> allBudget = budgetRepository.findAll();
+        Double amount = 0d;
+
+        for (Budget budget: allBudget){
+            if (isBetweenDate(start, end, budget.getMonth())) {
+                amount += calculateAmount(start,end, budget);
+            }
+        }
+        return amount;
+    }
+
+    public Double calculateAmount(LocalDate start, LocalDate end, Budget budget) {
+        LocalDate budgetDate = budget.getMonth();
+        if (!(start.getMonth() == budgetDate.getMonth() ||
+                end.getMonth() == budgetDate.getMonth())
+                ) {
+            return budget.getAmount();
+        } else if (start.getMonth() == budgetDate.getMonth()) {
+            if (end.getMonth() == budgetDate.getMonth()) {
+                return budget.getAmount() * (end.getDayOfMonth() - start.getDayOfMonth() + 1) / start.lengthOfMonth();
+            } else {
+                return budget.getAmount() * (start.lengthOfMonth() - start.getDayOfMonth() + 1) / start.lengthOfMonth();
+            }
+        } else {
+            return budget.getAmount() * (end.getDayOfMonth()) / end.lengthOfMonth();
+        }
+    }
+
+    public boolean isBetweenDate(LocalDate start, LocalDate end, LocalDate budgetMonth) {
+        LocalDate startBudget = budgetMonth.withDayOfMonth(1);
+        LocalDate endBudget = budgetMonth.withDayOfMonth(budgetMonth.lengthOfMonth());
+
+        return (!(start.isBefore(startBudget)) && !(start.isAfter(endBudget))) ||
+                (!(end.isBefore(startBudget)) && !(end.isAfter(endBudget)));
+    }
+
 }
--- src/test/java/com/odde/bbuddy/domain/BudgetsTest.java
+++ src/test/java/com/odde/bbuddy/domain/BudgetsTest.java
@@ -2,13 +2,19 @@ package com.odde.bbuddy.domain;
 
 import com.odde.bbuddy.repository.Budget;
 import com.odde.bbuddy.repository.BudgetRepository;
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -16,13 +22,18 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(JUnitParamsRunner.class)
 public class BudgetsTest {
     @Mock
     BudgetRepository budgetRepository;
     @InjectMocks
     Budgets budgets;
     private List<Budget> existingBudgetList;
+
+    @Before
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+    }
 
     private void givenBudgets(Budget... budgetList) {
         existingBudgetList = Arrays.asList(budgetList);
@@ -47,4 +58,70 @@ public class BudgetsTest {
         assertThat(budgetList).isEqualTo(existingBudgetList);
     }
 
+    @Test
+    @Parameters(
+            {
+                    "2018-07-01, 2018-07-31, 310",
+                    "2018-07-10, 2018-07-31, 220",
+                    "2018-06-01, 2018-07-31, 610",
+                    "2018-06-01, 2018-07-30, 600",
+                    "2018-06-02, 2018-07-30, 590"
+            })
+    public void search_total_amount(String startSearch, String endSearch, Double expectedAmount) {
+        List<Budget> budgetList = new ArrayList<>();
+        budgetList.add(new Budget(LocalDate.of(2018, 6, 1), 300d));
+        budgetList.add(new Budget(LocalDate.of(2018, 7, 1), 310d));
+        budgetList.add(new Budget(LocalDate.of(2018, 8, 1), 310d));
+
+        when(budgetRepository.findAll()).thenReturn(budgetList);
+
+        Double amount = budgets.searchBudget(startSearch, endSearch);
+
+        assertThat(amount).isEqualTo(expectedAmount);
+    }
+
+
+    @Test
+    @Parameters(
+            {
+                    "2018-07-01, 2018-07-31, 2018-07-01, true",
+                    "2018-07-10, 2018-07-31, 2018-07-01, true",
+                    "2018-07-01, 2018-07-31, 2018-07-01, true",
+                    "2018-07-01, 2018-07-31, 2018-08-01, false",
+                    "2018-07-01, 2018-08-31, 2018-06-01, false"
+            })
+    public void test_is_between_date(String startSearch, String endSearch, String budgetDate, Boolean expectingResult) {
+        LocalDate start = LocalDate.parse(startSearch);
+        LocalDate end = LocalDate.parse(endSearch);
+        LocalDate target = LocalDate.parse(budgetDate);
+
+        boolean isBetween = budgets.isBetweenDate(start, end, target);
+        assertThat(isBetween).isEqualTo(expectingResult);
+    }
+
+    @Test
+    public void testAbc() {
+        System.out.println(LocalDate.of(2018, 7, 1).isBefore(LocalDate.of(2018, 7, 1)));
+    }
+
+    @Test
+    @Parameters(
+            {
+                    "2018-07-01, 2018-07-31, 2018-07-01, 310",
+                    "2018-07-10, 2018-07-31, 2018-07-01, 220",
+                    "2018-07-01, 2018-07-15, 2018-07-01, 150",
+                    "2018-07-10, 2018-07-15, 2018-07-01, 60",
+                    "2018-06-01, 2018-08-31, 2018-07-01, 310",
+                    "2018-06-01, 2018-08-10, 2018-08-01, 100"
+            })
+    public void test_calculate_amount(String startSearch, String endSearch, String budgetMonth, Double expectedAmount) {
+        LocalDate start = LocalDate.parse(startSearch);
+        LocalDate end = LocalDate.parse(endSearch);
+        LocalDate budget = LocalDate.parse(budgetMonth);
+        Double amount = 310d;
+
+        Double resultAmount = budgets.calculateAmount(start, end, new Budget(budget, amount));
+
+        assertThat(resultAmount).isEqualTo(expectedAmount);
+    }
 }
